e315e7f964e34eef8177447988d84998633a3ec2
Jose Benitez <pepebenitez12@gmail.com>
2021-02-16 15:05:38 -0500

    First commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Conoce qué restricciones de movilidad están vigentes en Panamá</h1>
+        <p>Nos parecía increible que no existiese después de tanto tiempo un lugar donde encontrar información actualizada sobres las restricciones de movilidad por COVID</p>
+        <p>Así que YOLO, lo hicimos <a href="#">nosotros</a> mismos</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Banner',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
