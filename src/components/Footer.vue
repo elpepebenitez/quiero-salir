be380a7ed12c79e4c6a65f6fc787691d18321432
Jose Benitez <pepebenitez12@gmail.com>
2021-02-21 18:30:46 -0500

    Fixed some ortography errors and fixed modal in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,14 +11,14 @@
               <b-row>
                 <b-col>
                   <p>Las infografías del MINSA son muy útiles, pero recuerda que lo que vale es lo establecido legalmente</p>
-                  <p>Tomen lo que aparece en esta página como una guía, pero si tienen dudas específicas sobre su situación por favor consulten con un profesional idóneo</p>
+                  <p>Tomen lo que aparece en esta página como una guía</p> 
+                  <p>Si tienen dudas específicas sobre su situación por favor consulten con un profesional idóneo</p>
 
                 </b-col>
                 <b-col>
                   <p>Si tienes alguna sugerencia para mejorar el contenido déjanoslo saber <b-button href="https://wa.me/50766786283" variant="success" size="sm" target="_blank" style="color: white; font-weight: 900; display: inline;"><i class="fab fa-whatsapp"></i></b-button>  </p>
                   <p>También agradecemos cualquier contribución para mantener esta página</p>
-                  <b-button v-b-modal.modal-center variant="warning" size="lg" id="dona" style="color: white; font-weight: 900;">Apóyanos</b-button>
-
+                  <b-button v-b-modal.modal-footer variant="warning" size="lg" id="dona" style="color: white; font-weight: 900;">Apóyanos</b-button>
                 </b-col>
               </b-row>
             </b-container>
@@ -26,6 +26,14 @@
         <!--Grid row-->
       </div>
       <!-- Grid container -->
+
+      <div>
+          <b-modal id="modal-footer" title="¡Hola!" ok-only centered>
+              <p class="my-4">No nos tienes que dar nada por usar esta página, pero cualquier aporte nos ayuda a mantenerla :)</p>
+              <p>La manera más fácil de apoyarnos es por yappy. Puedes enviar al +507 66786283 con el mensaje "quiero salir"</p>
+          </b-modal>
+      </div>
+
 
       <!-- Copyright -->
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
@@ -49,5 +57,6 @@
     color: white;
     text-align: justify;
     text-justify: inter-word;
+    text-align: left;
   }
 </style>
